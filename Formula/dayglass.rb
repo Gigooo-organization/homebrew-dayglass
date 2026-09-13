017884073ebfa1b41bc5222083eeacdf8748df73
staticWagomU <to6wa17@gmail.com>
2026-09-14 01:42:25 +0900

    fix: let homebrew derive formula version

--- Formula/dayglass.rb
+++ Formula/dayglass.rb
@@ -2,7 +2,6 @@ class Dayglass < Formula
   desc "Local-first work-time evidence and reporting for macOS"
   homepage "https://github.com/Gigooo-organization/dayglass"
   url "https://github.com/Gigooo-organization/dayglass/archive/refs/tags/v0.1.1.tar.gz"
-  version "0.1.1"
   sha256 "534d513fa43e3d1b2e6940371462cac77d22e25826ca26b66763e7ca12ae6746"
   license "MIT"
 
