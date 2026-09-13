08846f204c8305739c659d0723fd2b243e7b0494
staticWagomU <to6wa17@gmail.com>
2026-09-14 01:38:06 +0900

    fix: satisfy homebrew formula ordering

--- Formula/dayglass.rb
+++ Formula/dayglass.rb
@@ -2,13 +2,13 @@ class Dayglass < Formula
   desc "Local-first work-time evidence and reporting for macOS"
   homepage "https://github.com/Gigooo-organization/dayglass"
   url "https://github.com/Gigooo-organization/dayglass/archive/refs/tags/v0.1.1.tar.gz"
+  version "0.1.1"
   sha256 "534d513fa43e3d1b2e6940371462cac77d22e25826ca26b66763e7ca12ae6746"
   license "MIT"
-  version "0.1.1"
 
+  depends_on "gh"
   depends_on arch: :arm64
   depends_on macos: :ventura
-  depends_on "gh"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
